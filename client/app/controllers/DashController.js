4749def3905929aa3ad88eca0a4fe27a08522d93
Nicholas Jakubowski <ncjakubowski@gmail.com>
2015-12-24 18:06:14 -0800

    Order bills by date

--- client/app/controllers/DashController.js
+++ client/app/controllers/DashController.js
@@ -2,19 +2,23 @@ angular.module('citizenfleet.dash', ['citizenfleet.services'])
   .controller('DashController', ['$scope', '$state', '$window', '$rootScope', 'DataService', function($scope, $state, $window, $rootScope, DataService) {
     
     $scope.userBills;
+    var user = $window.localStorage['isIt'];
     
     $scope.goToSearch = function() {
       $state.go('index');
     };
 
     $scope.loadBills = function() {
-      user = $window.localStorage['isIt'];
       DataService.showTrackedBills(user)
       .then(function(bills) {
         console.log(bills);
         $scope.userBills = bills;
       })
-    }
+    };
+
+    $scope.remove = function() {
+      
+    };
     
     //this is also in user controller can be
     //refactored for DRY
