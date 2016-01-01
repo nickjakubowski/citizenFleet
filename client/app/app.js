86f131534ae5567653b2e6a495e4df5121d685ab
Nicholas Jakubowski <ncjakubowski@gmail.com>
2015-12-31 20:08:42 -0800

    Setup route file

---
+++ client/app/app.js
@@ -0,0 +1,8 @@
+angular.module('citizenfleet')
+.run(runBlock);
+
+runBlock.$inject = ['$localStorage', '$rootscope','$state','$window'];
+
+function runBlock ($localStorage, $rootscope, $state, $window) {
+  
+}
