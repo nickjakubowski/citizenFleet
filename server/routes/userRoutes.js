3b2d2ede3bd74a17ab272e8e82d7b547ad70376a
Nicholas Jakubowski <ncjakubowski@gmail.com>
2015-12-31 19:54:39 -0800

    Change app.js to routes.js add app.js for config file

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ var userController = require('./userController.js');
 
 //from Yugen
 module.exports = function (app) {
+  // app.get('/favicon.ico', console.log("no favicon available"));	
   app.post('/login', userController.login);
   app.post('/signup', userController.signup);
   app.post('/index', userController.findBills);
